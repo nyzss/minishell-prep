bd400d79802c57d2abcaf3aa7d30b5128c7ec5a9
Okan Emre KOCA <oknakoca@gmail.com>
2024-06-28 11:50:08 +0200

    fix: decrementation was needed

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 14:53:22 by okoca             #+#    #+#             */
-/*   Updated: 2024/06/28 11:46:19 by okoca            ###   ########.fr       */
+/*   Updated: 2024/06/28 11:49:27 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -144,7 +144,6 @@ char	**split_once(char *value)
 		s2_len++;
 	if (s2_len == 0)
 		return (NULL);
-	// s2_len++;
 	s1 = malloc(sizeof(char) * (s1_len + 1));
 	s2 = malloc(sizeof(char) * (s2_len + 1));
 	while (value[i])
@@ -158,6 +157,7 @@ char	**split_once(char *value)
 		}
 		i++;
 	}
+	s2_len--;
 	s1[s1_len] = '\0';
 	s2[s2_len] = '\0';
 	printf("s1_len: %d - s2_len: %d\n", s1_len, s2_len);
